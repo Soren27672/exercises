2c7d9413d6f334b1fc5d7f5578666ccc24b56533
Maxwell Gottesman <maxgman16@yahoo.com>
2022-11-02 20:23:11 -0400

    +Exercise Random Selector

---
+++ Selector.js
@@ -0,0 +1,28 @@
+
+const selected = document.getElementById('selected');
+const exercises = ['Arrow functions! ','Switch! ','Splice! ','Map! ','Reduce! ','Find! ','Filter! ','Spread! ','Rest! ']
+
+function selectExercises(number = 5) {
+    const toDo = [];
+    const done = [];
+    for (let i = 0;i < number;++i) {
+        let suggestion = randomElement(exercises);
+        while (done.includes(suggestion)) {
+           suggestion = randomElement(exercises);
+        }
+        toDo.push(suggestion);
+        done.push(suggestion);
+    }
+    return toDo;
+}
+
+selected.textContent = selectExercises().reduce((ac,cv) => ac += cv);
+
+function randomElement(array) {
+    return array[randomInt(0,array.length-1)];
+}
+
+function randomInt(min,max) {
+    const diff = (max)-min;
+    return min + Math.round(diff * Math.random());
+}
